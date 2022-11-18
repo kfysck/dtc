4cc38dd733df412c416c5286d21cc6a2fb41e189
yangshuang70 <yangshuang68@jd.com>
2022-11-18 15:46:31 +0800

    test

--- dockerfiles/devel/run.sh
+++ dockerfiles/devel/run.sh
@@ -5,14 +5,6 @@ sleep_count=0
 while [ $sleep_count -le 100 ]
 do
     if [ -f "/usr/local/dtc/conf/dtc.yaml" ]; then 
-        mkdir /usr/local/dtc/conf
-        cp /usr/local/etc/conf/log4cplus.conf /usr/local/dtc/conf/
-        cp /usr/local/etc/conf/log4cplus-hwc.conf /usr/local/dtc/conf/
-        cp /usr/local/etc/conf/log4cplus-wd.conf /usr/local/dtc/conf/
-        cp /usr/local/etc/conf/log4cplus-agent.conf /usr/local/dtc/conf/
-        cp /usr/local/etc/conf/log4cplus-async.conf /usr/local/dtc/conf/
-        cp /usr/local/etc/conf/log4cplus-life.conf /usr/local/dtc/conf/
-        cp /usr/local/etc/conf/my.conf /usr/local/dtc/conf/
         echo "Start running process: "$DTC_BIN","$DTC_ARGV
         if [ $DTC_ARGV == "-a" ]; then
             echo "sleeping for agent"
